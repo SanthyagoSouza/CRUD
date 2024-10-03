63d5e0220b51ed3d1c79af897b7156097ff36afd
Santhyago Souza <santhyagoponciano@gmail.com>
2024-10-02 22:09:38 -0300

    delete

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 $pdo = new PDO('mysql:host=localhost;dbname=pd_bd_jc145', 'root', '');
 //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //insert.
+
+
+if (isset($_GET ["delete"])) {
+    $id = (int)$_GET['delete'];
+    $pdo->exec("DELETE FROM cliente WHERE idcliente=$id"); 
+    echo"Deletado com sucesso";
+}
+
 if (isset($_POST ["nome"])) {  
     
     $sql = $pdo-> prepare("INSERT INTO cliente (nome, sobrenome, telefone) VALUES (?,?,?)");
@@ -24,3 +32,17 @@ if (isset($_POST ["nome"])) {
 <input type="submit" name="enviar">
 
 </form>
+
+<?php
+
+$sql = $pdo->prepare("SELECT * FROM cliente");
+$sql->execute();
+
+$fetchClientes = $sql-> fetchAll();
+
+foreach ($fetchClientes as $key => $value) {
+    echo '<a href=?delete='.$value['idcliente'] ."'>(x)</a>" .$value ["nome"] ."|". $value["sobrenome"] ."|". $value["telefone"] ;
+    echo"<hr>";
+}
+
+?>
